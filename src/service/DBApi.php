8398c3066fa7c3691bd30bc858ee51c3e26291fc
stanislaw-ch <post.chirko@gmail.com>
2023-01-26 10:06:35 +0100

    #17 refactor: use parameterized queries

--- src/service/DBApi.php
+++ src/service/DBApi.php
@@ -15,7 +15,7 @@ class DBApi
             ";
             $result = DBConfig::connect()->query($query);
 
-            for ($results = array(); $row = $result->fetch_assoc(); $results[] = $row) ;
+            for ($results = array(); $row = $result->fetch_assoc(); $results[] = $row);
         } catch (Exception $error) {
             onError($error);
         }
@@ -45,8 +45,8 @@ class DBApi
     {
         $results = [];
         try {
-            $query = "SELECT $table.id, gender FROM $table 
-                        LEFT JOIN employees e ON e.gender_id = $table.id 
+            $query = "SELECT $table.id, gender FROM $table
+                        LEFT JOIN employees e ON e.gender_id = $table.id
                         GROUP BY $table.id";
             $result = DBConfig::connect()->query($query);
 
@@ -58,24 +58,12 @@ class DBApi
         return $results;
     }
 
-    public function create($table, $values): void
+    public function createDepartment($table, $values): void
     {
         try {
-            $query = "INSERT INTO $table (";
-
-            foreach ($values as $field => $value) $query .= $field . ", ";
-            $query = substr($query, 0, -2);
-            $query .= ") VALUES (";
-
-            foreach ($values as $value) {
-                $query .= is_numeric($value)
-                    ? addslashes($value) . ","
-                    : "'" . addslashes($value) . "',";
-            };
-            $query = substr($query, 0, -1);
-            $query .= ")";
-
-            DBConfig::connect()->query($query);
+            $stmt = DBConfig::connect()->prepare("INSERT INTO $table(name) VALUES (?)");
+            $stmt->bind_param("s", $values['name']);
+            $stmt->execute();
         } catch (Exception $error) {
             $message = $table === 'employees'
                 ? $values['firstname'] . ' ' . $values['lastname'] . ' ist schon vorhanden'
@@ -83,30 +71,75 @@ class DBApi
             onError($error, $message);
         }
     }
+
+    public function createEmployee($table, $values): void
+    {
+        try {
+            $stmt = DBConfig::connect()->prepare(
+                "INSERT INTO $table(firstname, lastname, salary, gender_id, department_id) 
+                 VALUES (?, ?, ?, ?, ?)"
+            );
+            $stmt->bind_param(
+                "ssiii",
+                $values['firstname'],
+                $values['lastname'],
+                $values['salary'],
+                $values['gender_id'],
+                $values['department_id']
+            );
+
+            $stmt->execute();
+        } catch (Exception $error) {
+            $message = $table === 'employees'
+                ? $values['firstname'] . ' ' . $values['lastname'] . ' ist schon vorhanden'
+                : $values['name'] . ' ist schon vorhanden';
+            onError($error, $message);
+        }
+    }
+
+//    public function create($table, $values): void
+//    {
+//        try {
+//            $query = "INSERT INTO $table (";
+//
+//            foreach ($values as $field => $value) $query .= $field . ", ";
+//            $query = substr($query, 0, -2);
+//            $query .= ") VALUES (";
+//
+//            foreach ($values as $value) {
+//                $query .= is_numeric($value)
+//                    ? addslashes($value) . ","
+//                    : "'" . addslashes($value) . "',";
+//            };
+//            $query = substr($query, 0, -1);
+//            $query .= ")";
+//
+//            DBConfig::connect()->query($query);
+//        } catch (Exception $error) {
+//            $message = $table === 'employees'
+//                ? $values['firstname'] . ' ' . $values['lastname'] . ' ist schon vorhanden'
+//                : $values['name'] . ' ist schon vorhanden';
+//            onError($error, $message);
+//        }
+//    }
 
     public function delete($table, $id): void
     {
         try {
-            $query = "DELETE FROM $table WHERE id = $id";
-            DBConfig::connect()->query($query);
+            $stmt = DBConfig::connect()->prepare("DELETE FROM $table WHERE id = ?");
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
         } catch (Exception $error) {
             onError($error);
         }
     }
 
-    public function update($table, $id, $values): void
+    public function updateDepartment($table, $id, $values): void
     {
         try {
-            $query = "UPDATE $table SET ";
-            foreach ($values as $field => $value) {
-                $query .= $field === 'id'
-                    ? ""
-                    : "$field = '" . addslashes($value) . "', ";
-            };
-            $query = substr($query, 0, -2);
-            $query .= " WHERE id = $id";
-
-            DBConfig::connect()->query($query);
+            $stmt = DBConfig::connect()->prepare("UPDATE $table SET name = ? WHERE id = ?");
+            $stmt->bind_param("si", $values['name'], $id);
+            $stmt->execute();
         } catch (Exception $error) {
             $message = $table === 'employees'
                 ? $values['firstname'] . ' ' . $values['lastname'] . ' ist schon vorhanden'
@@ -115,28 +148,79 @@ class DBApi
         }
     }
 
+    public function updateEmployee($table, $id, $values): void
+    {
+        try {
+            $stmt = DBConfig::connect()->prepare(
+                "UPDATE $table 
+                 SET firstname = ? , lastname = ?, salary = ?, gender_id = ?, department_id = ? WHERE id = ?"
+            );
+            $stmt->bind_param(
+                "ssiiii",
+                $values['firstname'],
+                $values['lastname'],
+                $values['salary'],
+                $values['gender_id'],
+                $values['department_id'],
+                $id
+            );
+            $stmt->execute();
+        } catch (Exception $error) {
+            $message = $table === 'employees'
+                ? $values['firstname'] . ' ' . $values['lastname'] . ' ist schon vorhanden'
+                : $values['name'] . ' ist schon vorhanden';
+            onError($error, $message);
+        }
+    }
+
+//    public function update($table, $id, $values): void
+//    {
+//        try {
+//            $query = "UPDATE $table SET ";
+//            foreach ($values as $field => $value) {
+//                $query .= $field === 'id'
+//                    ? ""
+//                    : "$field = '" . addslashes($value) . "', ";
+//            };
+//            $query = substr($query, 0, -2);
+//            $query .= " WHERE id = $id";
+//
+//            DBConfig::connect()->query($query);
+//        } catch (Exception $error) {
+//            $message = $table === 'employees'
+//                ? $values['firstname'] . ' ' . $values['lastname'] . ' ist schon vorhanden'
+//                : $values['name'] . ' ist schon vorhanden';
+//            onError($error, $message);
+//        }
+//    }
+
     public function getById($table, $id): bool|array|null
     {
         try {
-            $query = "SELECT * FROM $table WHERE id = $id";
-            return DBConfig::connect()->query($query)->fetch_assoc();
+            $stmt = DBConfig::connect()->prepare("SELECT * FROM $table WHERE id = ?");
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            return $result->fetch_assoc();
         } catch (Exception $error) {
             onError($error);
         }
-
-        print_r(DBConfig::connect()->query($query)->fetch_assoc());
-
         return false;
     }
 
     public function getEmployeeById($table, $id): bool|array|null
     {
         try {
-            $query = "SELECT $table.id, firstname, lastname, salary, gender, gender_id, department_id FROM $table 
-                        JOIN genders g ON g.id = $table.gender_id                  
-                        AND $table.id = $id
-                        GROUP BY $table.id";
-            return DBConfig::connect()->query($query)->fetch_assoc();
+            $stmt = DBConfig::connect()->prepare(
+                "SELECT $table.id, firstname, lastname, salary, gender, gender_id, department_id 
+                 FROM $table 
+                 JOIN genders g ON g.id = $table.gender_id                  
+                 AND $table.id = ?
+                 GROUP BY $table.id");
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            return $result->fetch_assoc();
         } catch (Exception $error) {
             onError($error);
         }
